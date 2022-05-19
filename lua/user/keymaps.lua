1143712b55c6069231964b07f90ae0a0c783e5e8
mp <mateus.cabete@gmail.com>
2022-05-19 11:27:30 -0300

    telescope keymappings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -68,3 +68,7 @@ keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- telescope --
+keymap("n", "<leader>f", ":Telescope find_files<CR>", opts)
+keymap("n", "<leader>g", ":Telescope live_grep<CR>", opts)
